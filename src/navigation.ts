a39d791c661df617abc83c49305399971fa2c037
Andrey Masloboev <amasloboev@topcon.com>
2025-12-04 02:53:48 +0300

    done about

--- src/navigation.ts
+++ src/navigation.ts
@@ -27,32 +27,8 @@ export const headerData = {
       text: 'О нас',
       links: [
         {
-          text: 'Features (Anchor Link)',
-          href: getPermalink('/#features'),
-        },
-        {
-          text: 'Services',
-          href: getPermalink('/services'),
-        },
-        {
-          text: 'Pricing',
-          href: getPermalink('/pricing'),
-        },
-        {
-          text: 'About us',
+          text: 'О нас',
           href: getPermalink('/about'),
-        },
-        {
-          text: 'Contact',
-          href: getPermalink('/contact'),
-        },
-        {
-          text: 'Terms',
-          href: getPermalink('/terms'),
-        },
-        {
-          text: 'Privacy policy',
-          href: getPermalink('/privacy'),
         },
       ],
     },
