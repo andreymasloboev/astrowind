43b1633df9c11bf8d81ff6d5c9abf594ded9d256
Andrey Masloboev <amasloboev@topcon.com>
2025-12-04 03:32:30 +0300

    added product oil

--- src/navigation.ts
+++ src/navigation.ts
@@ -6,20 +6,12 @@ export const headerData = {
       text: 'Продукция',
       links: [
         {
-          text: 'SaaS',
-          href: getPermalink('/homes/saas'),
+          text: 'Рапсовое масло',
+          href: getPermalink('/homes/oil'),
         },
         {
-          text: 'Startup',
-          href: getPermalink('/homes/startup'),
-        },
-        {
-          text: 'Mobile App',
-          href: getPermalink('/homes/mobile-app'),
-        },
-        {
-          text: 'Personal',
-          href: getPermalink('/homes/personal'),
+          text: 'Рапсовое жмых',
+          href: getPermalink('/homes/gmuh'),
         },
       ],
     },
@@ -38,31 +30,6 @@ export const headerData = {
         {
           text: 'Контакты',
           href: getPermalink('/contact'),
-        },
-      ],
-    },
-    {
-      text: 'Blog',
-      links: [
-        {
-          text: 'Blog List',
-          href: getBlogPermalink(),
-        },
-        {
-          text: 'Article',
-          href: getPermalink('get-started-website-with-astro-tailwind-css', 'post'),
-        },
-        {
-          text: 'Article (with MDX)',
-          href: getPermalink('markdown-elements-demo-post', 'post'),
-        },
-        {
-          text: 'Category Page',
-          href: getPermalink('tutorials', 'category'),
-        },
-        {
-          text: 'Tag Page',
-          href: getPermalink('astro', 'tag'),
         },
       ],
     },
